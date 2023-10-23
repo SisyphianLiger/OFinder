820d754f554fee357df09c563f429c4be8c4ff5b
SisyphianLiger <69813075+SisyphianLiger@users.noreply.github.com>
2023-10-23 21:51:57 +0200

    Added First Test for Matrix

---
+++ Ofinder/test/Ofinder.ml
@@ -0,0 +1,10 @@
+open Base
+open Lib.Ls
+
+let%test "rev" =
+    List.equal Int.equal (List.rev [ 3; 2; 1 ]) [ 1; 2; 3 ];;
+
+let%test "2D Matrices are the Same" = 
+    let std_matrix = Array.make_matrix ~dimx:4 ~dimy:4 0 in
+    let my_matrix = make_str_matrix 4 4 in
+    Array.equal (Array.equal Int.equal) std_matrix my_matrix
