d0dc5cdf2b02bd4c0d3c7480f82c86141b5836ff
SisyphianLiger <69813075+SisyphianLiger@users.noreply.github.com>
2023-11-19 22:26:11 +0100

    Progressing to another solution with key up/ key down to finish this project

--- Ofinder/_build/default/lib/.lib.objs/lib__Loop_nav.impl.d
+++ Ofinder/_build/default/lib/.lib.objs/lib__Loop_nav.impl.d
@@ -1,1 +1,1 @@
-lib/Loop_nav.ml: Char Cli Curses List Ls Match_str String
+lib/Loop_nav.ml: A Char Cli Curses List Ls Match_str String
--- Ofinder/_build/default/lib/Loop_nav.ml
+++ Ofinder/_build/default/lib/Loop_nav.ml
@@ -26,9 +26,19 @@ open Match_str
 
 
  *)
+(* supposed window initialization happened before this line *)
+let print_in_red win str = 
+    let _ = start_color () in
+    let _ = init_pair 1 1 0 in (* initializing red *)
+    wattron win (A.color_pair 1); (* turning on color *)
+    ignore (waddstr win str); (* printing the string *)
+    wattroff win A.(color_pair 1); (* turning off color *)
+    refresh ()
 
 (* Time to make the str_match Red *)
 let str_colorizer win str ss = 
+        let _ = colors () in
+        let _ = init_pair 1 2 0 in
         match ss with
         | None                                   ->  str
         | Some (x,y)    ->  
@@ -36,17 +46,14 @@ let str_colorizer win str ss =
                             let tuple_two = y in 
                             let str_a_len = String.length str in 
                             let first_string = String.sub str 0 tuple_one in 
-                            let _ = waddstr win first_string in
+                            wattron win (A.color_pair 1);
                             let highlighted = String.sub str tuple_one (tuple_two - tuple_one) in
-                            let _ = waddstr win highlighted in
+                            wattroff win (A.color_pair 1);
                             let second_string = String.sub str tuple_two (str_a_len - tuple_two) in 
-                            first_string ^ " || " ^ highlighted ^ " || " ^ second_string
+                            first_string ^ " ==> " ^highlighted ^ " <== " ^ second_string
 
 
-let unpacker_str x = 
-    match x with 
-    | None   -> ""
-    | Some x -> string_of_int x
+let unpacker_str x = if x = 0 then "F" else "D"
 
 
 (* Helper Function for sorting *)
@@ -92,6 +99,7 @@ let window_search path =
     let backspace = 127 in 
     let escape = 27 in
 
+
     let ls_loop = ref true in
     let s_input = ref "" in
     while !ls_loop do 
@@ -102,6 +110,14 @@ let window_search path =
                                             s_input := String.sub !s_input 0 (String.length !s_input - 1);
                                             ignore(mvaddstr ((maxy * 9 / 10) + 1) 2 (String.make maxx ' '));
                                             ignore(mvaddstr ((maxy * 9 / 10) + 1) 3 !s_input);
+                                            List.iter (fun x -> x.ls_score <- Some(make_str_matrix !s_input x.fd_str);
+                                                                x.sub_str_pnt <- my_match_str x.fd_str !s_input )f_d_found;
+                                            (* Now we sort :( *)
+                                            List.sort compare_ls f_d_found |> List.iteri (fun i x -> if i < input_range 
+                                            then ignore(mvaddstr (input_range - i + 2) 2 ((unpacker_str x.file_or_dir) ^ " " ^  
+                                            (str_colorizer win_top x.fd_str x.sub_str_pnt))));
+                                            let _ = wrefresh win_top in 
+                                            ls_loop := true
 
         | key when key = escape                         ->   ls_loop := false
 
@@ -112,7 +128,7 @@ let window_search path =
                                                                             x.sub_str_pnt <- my_match_str x.fd_str !s_input )f_d_found;
                                                              (* Now we sort :( *)
                                                              List.sort compare_ls f_d_found |> List.iteri (fun i x -> if i < input_range 
-                                                             then ignore(mvaddstr (input_range - i + 2) 2 ((unpacker_str x.ls_score) ^ " " ^  
+                                                             then ignore(mvaddstr (input_range - i + 2) 2 ((unpacker_str x.file_or_dir) ^ " " ^  
                                                              (str_colorizer win_top x.fd_str x.sub_str_pnt))
 
                                                              (* Here is where we change color based on str match *)
